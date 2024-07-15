7805a0b143b9aacd854db5f7fc8d6b55b3ef7acc
Adeel Akram <adeel@swishtag.org>
2024-07-15 20:51:38 +0500

    Minor Changes !!!

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -21,7 +21,6 @@ class DashboardController extends Controller
     public function index(Request $request)
     {
         $user = Auth::user();
-        dd('DashboardController@index is called.');
         Log::info('DashboardController@index is called.');
         // $user->api()->rest('POST', '/admin/api/2024-01/webhooks.json', [
         //     'webhook' => [
