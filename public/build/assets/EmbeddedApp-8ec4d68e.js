065ca200e78d2d25e07f4108922823124eed84c6
Adeel Akram <adeel@swishtag.org>
2024-07-15 20:22:01 +0500

    minor changes

---
+++ public/build/assets/EmbeddedApp-8ec4d68e.js
@@ -0,0 +1,1 @@
+import{C as o,j as p}from"./app-7acc9119.js";const d=()=>{const e=o().props;return console.log(e),p.jsx("div",{children:"Embedded App"})};export{d as default};
--- public/build/assets/EmbeddedApp-ac4f15da.js
+++
@@ -1,1 +0,0 @@
-import{C as o,j as p}from"./app-41a1fc54.js";const d=()=>{const e=o().props;return console.log(e),p.jsx("div",{children:"Embedded App"})};export{d as default};
