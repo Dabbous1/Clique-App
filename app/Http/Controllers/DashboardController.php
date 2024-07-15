ff3ffcff19abc3e34e5597e5d09996bf504c21df
Adeel Akram <adeel@swishtag.org>
2024-07-15 18:33:34 +0500

    minor loggings

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -21,7 +21,7 @@ class DashboardController extends Controller
     public function index(Request $request)
     {
         $user = Auth::user();
-
+        Log::info('DashboardController@index is called.');
         // $user->api()->rest('POST', '/admin/api/2024-01/webhooks.json', [
         //     'webhook' => [
         //         'topic' => 'products/delete',
